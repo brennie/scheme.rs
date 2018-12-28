7eb8e30ecc4553797ff05f9e803f8b99569c2911
Barret Rennie <barret@brennie.ca>
2018-12-28 13:35:42 -0600

    Implement lexer for string literals

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,7 +5,7 @@
 // or http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use std::fmt;
+use std::{borrow::Cow, fmt};
 
 use combine::{
     parser::{
@@ -13,8 +13,9 @@ use combine::{
         choice::{choice, or},
         combinator::{attempt, look_ahead},
         item::{any, eof, item, satisfy},
-        range::range,
+        range::{range, recognize},
         repeat::skip_many,
+        sequence::between,
     },
     stream::{
         state::{Positioner, RangePositioner, State},
@@ -107,7 +108,7 @@ pub struct TokenIter<'a> {
 }
 
 impl<'a> Iterator for TokenIter<'a> {
-    type Item = Result<Token, <State<&'a str, LinePositioner> as StreamOnce>::Error>;
+    type Item = Result<Token<'a>, <State<&'a str, LinePositioner> as StreamOnce>::Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let remaining = self.remaining.take()?;
@@ -133,7 +134,7 @@ impl<'a> Iterator for TokenIter<'a> {
 
 /// A parsed token.
 #[derive(Clone, Debug, PartialEq)]
-pub enum Token {
+pub enum Token<'a> {
     /// An open parenthesis.
     Open,
     /// A close parenthesis.
@@ -144,10 +145,12 @@ pub enum Token {
     Bool(bool),
     /// A character value.
     Char(char),
+    /// A string literal.
+    Str(Cow<'a, str>),
 }
 
 /// Parse a token.
-fn token<'a, I>() -> impl Parser<Input = I, Output = Token> + 'a
+fn token<'a, I>() -> impl Parser<Input = I, Output = Token<'a>> + 'a
 where
     I: RangeStream<Item = char, Range = &'a str> + 'a,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
@@ -158,6 +161,7 @@ where
         item('.').skip(look_ahead(delimiter())).map(|_| Token::Dot),
         attempt(bool_lit()).map(Token::Bool),
         attempt(char_lit()).map(Token::Char),
+        string_lit().map(Token::Str),
     ))
 }
 
@@ -183,6 +187,58 @@ where
             any(),
         )))
         .skip(look_ahead(delimiter()))
+}
+
+/// Parse a string literal.
+///
+/// If the string contains no escapes, this will not do any allocations.
+fn string_lit<'a, I>() -> impl Parser<Input = I, Output = Cow<'a, str>> + 'a
+where
+    I: RangeStream<Item = char, Range = &'a str> + 'a,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    between(
+        item('"'),
+        item('"'),
+        recognize(skip_many(or(
+            satisfy(|c| c != '"' && c != '\\'),
+            item('\\').with(satisfy(|c| c == '"' || c == '\\')),
+        ))),
+    )
+    .map(|s: &'a str| {
+        if s.find('\\').is_some() {
+            let s = s
+                .chars()
+                .scan(false, |escaped, c| match (*escaped, c) {
+                    (true, '\\') => {
+                        *escaped = false;
+                        Some(Some('\\'))
+                    }
+
+                    (true, '"') => {
+                        *escaped = false;
+                        Some(Some('"'))
+                    }
+
+                    (true, _) => unreachable!("Invalid escape sequence"),
+
+                    (false, '\\') => {
+                        *escaped = true;
+                        Some(None)
+                    }
+
+                    (false, c) => {
+                        *escaped = false;
+                        Some(Some(c))
+                    }
+                })
+                .filter_map(|c| c)
+                .collect::<String>();
+            Cow::Owned(s)
+        } else {
+            Cow::Borrowed(s)
+        }
+    })
 }
 
 /// Return whether or not a character is a delimiter.
@@ -264,5 +320,25 @@ mod test {
         assert_eq!(token().parse("#\\a"), Ok((Token::Char('a'), "")));
         assert_eq!(token().parse("#\\4"), Ok((Token::Char('4'), "")));
         assert_eq!(token().parse("#\\*"), Ok((Token::Char('*'), "")));
+    }
+
+    #[test]
+    fn test_token_string() {
+        assert_eq!(
+            token().parse(r#""""#),
+            Ok((Token::Str(Cow::Borrowed("")), ""))
+        );
+        assert_eq!(
+            token().parse(r#""a""#),
+            Ok((Token::Str(Cow::Borrowed("a")), ""))
+        );
+        assert_eq!(
+            token().parse(r#""\\""#),
+            Ok((Token::Str(Cow::Owned("\\".into())), ""))
+        );
+        assert_eq!(
+            token().parse(r#""\"""#),
+            Ok((Token::Str(Cow::Owned("\"".into())), ""))
+        );
     }
 }
